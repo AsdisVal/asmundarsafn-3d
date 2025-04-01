318086534ec6fa0b960cecaa1e9d50c6feaacfa5
Ásdís Valtýsdóttir <AsdisVal@users.noreply.github.com>
2025-04-01 04:15:44 +0000

    einstaklings

--- main.js
+++ main.js
@@ -78,129 +78,16 @@ ground.rotation.x = -Math.PI / 2; // make it horizontal
 ground.position.y = 0;
 scene.add(ground);
 
-// -----------------------------
-// Building Group
-// -----------------------------
-const building = new THREE.Group();
-scene.add(building);
-
-const textureLoader = new THREE.TextureLoader();
-
-const frontWallTexture = textureLoader.load('texture/front_wall.jpg');
-const otherTexture = textureLoader.load('texture/asmundarsafn_white.jpg');
-const leftWallTexture = textureLoader.load('texture/left_side_wall.jpg');
-const rightWallTexture = textureLoader.load('texture/right_side_wall.jpg');
-
-const frontMaterial = new THREE.MeshLambertMaterial({ map: frontWallTexture });
-const otherMaterial = new THREE.MeshLambertMaterial({ map: otherTexture });
-const leftMaterial = new THREE.MeshLambertMaterial({ map: leftWallTexture });
-const rightMaterial = new THREE.MeshLambertMaterial({ map: rightWallTexture });
-
-const materials = [
-  rightMaterial, // Right side
-  leftMaterial, // Left side
-  otherMaterial, // Top side
-  otherMaterial, // Bottom side
-  frontMaterial, // Front side
-  otherMaterial, // Back side
-];
-const boxGeometry = new THREE.BoxGeometry(9.4, 4.5, 14);
-boxGeometry.translate(0, 1.3, 13); // shift so back is at z=-3, front at z=4.5
-const boxmesh = new THREE.Mesh(boxGeometry, materials);
-boxmesh.position.y = 1; // so bottom sits at y=0
-building.add(boxmesh);
-
-// 2) Hvíta kúlan (hæð)=4.5
-const domeRadius = 4.5;
-const domeGeometry = new THREE.SphereGeometry(
-  domeRadius,
-  32,
-  16,
-  0,
-  Math.PI * 2,
-  0,
-  Math.PI / 1.9
-);
-const boxMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-const dome = new THREE.Mesh(domeGeometry, boxMaterial);
-dome.translateZ(11);
-dome.position.y = 4.2;
-building.add(dome);
-
-// 3) Trapisurnar h-megin og v-megin (hæð)=2.5
-const trapezoidShape = new THREE.Shape();
-trapezoidShape.moveTo(-6, 0); // bottom left
-trapezoidShape.lineTo(6, 0); // bottom right
-trapezoidShape.lineTo(4, 6); // top right
-trapezoidShape.lineTo(-4, 6); // top left
-trapezoidShape.lineTo(-6, 0); // close the shape
-
-const extrudeSettings = {
-  steps: 1,
-  depth: 5,
-  bevelEnabled: false,
-};
-const trapezoidGeometry = new THREE.ExtrudeGeometry(
-  trapezoidShape,
-  extrudeSettings
-);
-
-const patternTexture = textureLoader.load('texture/asmundarsafn_white.jpg');
-patternTexture.wrapS = THREE.RepeatWrapping;
-patternTexture.wrapT = THREE.RepeatWrapping;
-patternTexture.repeat.set(1, 1);
-
-const patternMaterial = new THREE.MeshLambertMaterial({ map: patternTexture });
-
-const leftSide = new THREE.Mesh(trapezoidGeometry, patternMaterial);
-leftSide.position.set(-9.1, 0, 15.5);
-building.add(leftSide);
-
-const rightSide = new THREE.Mesh(trapezoidGeometry.clone(), patternMaterial);
-rightSide.position.set(9.1, 0, 15.5);
-building.add(rightSide);
-
-// aftari parturinn
-const backBox = new THREE.BoxGeometry(9.4, 3, 16);
-backBox.translate(0, 1, 0); // shift so back is at z=-3, front at z=4.5
-const backBoxMesh = new THREE.Mesh(backBox, materials);
-backBoxMesh.position.y = 1; // so bottom sits at y=0
-backBoxMesh.position.z = -1;
-building.add(backBoxMesh);
-// Create a circle geometry that covers 70% of a full circle
-const radius = 5;
-const segments = 32;
-const thetaStart = 0;
-const thetaLength = 2 * Math.PI * 0.4; // 70% of 2π
-
-const geometry = new THREE.CircleGeometry(
-  radius,
-  segments,
-  thetaStart,
-  thetaLength
-);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  side: THREE.DoubleSide,
-});
-const circleMesh = new THREE.Mesh(geometry, material);
-circleMesh.rotateX(Math.PI / 2);
-circleMesh.rotateZ(Math.PI * 1.1);
-// make circle bigger
-circleMesh.scale.set(4, 4, 4);
-circleMesh.position.set(0, 2.5, 8);
-building.add(circleMesh);
-
 // ad a model of the buillding instead
 // add mtl loader and also obj loader
 const mtlLoader = new MTLLoader();
-mtlLoader.load('asmundarsafn/asmundarsafni.mtl', (materials) => {
+mtlLoader.load('asmundarsafn/asmundarsafn_cpy_to_test_11.mtl', (materials) => {
   const objLoader = new OBJLoader();
   objLoader.setMaterials(materials);
-  objLoader.load('asmundarsafn/asmundarsafni.obj', (object) => {
-    object.position.set(10, 10, 10);
-    object.scale.set(3, 3, 3);
-    object.rotation.set(0, 0, 0);
+  objLoader.load('asmundarsafn/asmundarsafn_cpy_to_test_11.obj', (object) => {
+    object.position.set(0, 5, 10);
+    object.scale.set(1.5, 1.5, 1.5);
+    object.rotation.y = Math.PI;
     scene.add(object);
   });
 });
